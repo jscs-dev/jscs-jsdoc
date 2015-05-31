8396a90dd7139e5ce25d603dd4d5a9610e0274ae
dtracers <dtracers@gmail.com>
2015-05-30 19:51:50 -0600

    added new rule: require-description-complete-sentence

---
+++ test/lib/rules/validate-jsdoc/require-description-complete-sentence.js
@@ -0,0 +1,105 @@
+describe('lib/rules/validate-jsdoc/require-description-complete-sentence', function () {
+    var checker = global.checker({
+        additionalRules: ['lib/rules/validate-jsdoc.js']
+    });
+
+    describe('not configured', function() {
+
+        it('should report with undefined', function() {
+            global.expect(function() {
+                checker.configure({requireDescriptionCompleteSentence: undefined});
+            }).to.throws(/accepted value/i);
+        });
+
+        it('should report with an object', function() {
+            global.expect(function() {
+                checker.configure({requireDescriptionCompleteSentence: {}});
+            }).to.throws(/accepted value/i);
+        });
+
+    });
+
+    describe('with true', function() {
+        checker.rules({requireDescriptionCompleteSentence: true});
+
+        checker.cases([
+            /* jshint ignore:start */
+            {
+                it: 'should not report common cases',
+                code: function() {
+                    function fun(p) {
+                    }
+
+                    /**
+                     * @param p
+                     */
+                    function fun(p) {
+                    }
+                }
+            }, {
+                it: 'should report missing period at end of description',
+                code: function () {
+                    /**
+                     * Some description
+                     * @param {number} p description without hyphen
+                     */
+                    function fun(p) {
+                    }
+                },
+                errors: {
+                    line: 2,
+                    column: 19
+                },
+            }, {
+                it: 'should report missing upper case at beginning of description',
+                code: function () {
+                    /**
+                     * some description.
+                     * @param {number} p description without hyphen
+                     */
+                    function fun(p) {
+                    }
+                },
+                errors: {
+                    line: 2,
+                    column: 4
+                },
+            }, {
+                it: 'should not report missing period or missing upper case letter',
+                code: function () {
+                    /**
+                     * Some description.
+                     *
+                     * @param {number} p description without hyphen
+                     */
+                    function fun(p) {}
+                }
+            }, {
+                it: 'should report missing period',
+                code: function () {
+                    /**
+                     * Some description .
+                     *
+                     * @param {number} p description without hyphen
+                     */
+                    function fun(p) {}
+                },
+                errors: 1
+            }, {
+                it: 'should not missing period',
+                code: function () {
+                    /**
+                     * Some description
+                     * Which is continued on the next line.
+                     * @param {number} p description without hyphen
+                     */
+                    function fun(p) {}
+                },
+                errors: 1
+            }
+            /* jshint ignore:end */
+        ]);
+
+    });
+
+});
